392ee4a5c1f3232d9169ae7dbc00b7ba91ea59cf
raffaele <raffaele.fontana.02@gmail.com>
2024-10-17 16:11:18 +0200

    feat: remove pipeline files

--- sigma/pipelines/surrealql/__init__.py
+++
@@ -1,6 +0,0 @@
-from .surrealql import surrealql_pipeline
-# TODO: add all pipelines that should be exposed to the user of your backend in the import statement above.
-
-pipelines = {
-    "surrealql_pipeline": surrealql_pipeline,   # TODO: adapt identifier to something approproiate
-}
--- tests/test_pipelines_surrealql.py
+++
@@ -1,5 +0,0 @@
-import pytest
-from sigma.backends.surrealql import SurrealQLBackend
-from sigma.pipelines.surrealql import # TODO: import pipeline functions
-
-# TODO: import tests for all implemented pipelines and contained transformations
